24ff3409d91cbcdb02a0904c7cc8f4330f8a1d6b
HMQ <hmquan191@gmail.com>
2025-03-04 15:39:49 +0700

    app bar p2

--- src/theme.js
+++ src/theme.js
@@ -1,13 +1,14 @@
 import { experimental_extendTheme as extendTheme } from "@mui/material/styles";
 
 import { deepOrange, orange, teal, cyan } from "@mui/material/colors";
-// Create a theme instance.
+import { BorderColor } from "@mui/icons-material";
 
 const theme = extendTheme({
   trello: {
-    appBarHeight: "48px",
-    boardBarHeight: "58px",
+    appBarHeight: "58px",
+    boardBarHeight: "60px",
   },
+
   colorSchemes: {
     light: {
       palette: {
@@ -19,6 +20,48 @@ const theme = extendTheme({
       palette: {
         primary: cyan,
         secondary: orange,
+      },
+    },
+  },
+
+  components: {
+    MuiButton: {
+      styleOverrides: {
+        root: {
+          textTransform: "none", // ve binh thuong ko ghi hoa
+        },
+      },
+    },
+
+    MuiInputLabel: {
+      styleOverrides: {
+        root: ({ theme }) => ({
+          color: theme.palette.primary.main,
+          fontSize: "0.875rem",
+        }),
+      },
+    },
+    MuiOutlinedInput: {
+      styleOverrides: {
+        root: ({ theme }) => {
+          return {
+            color: theme.palette.primary.main,
+            fontSize: "0.875rem",
+            // duong vien ngoai cua o search
+            ".MuiOutlinedInput-notchedOutline": {
+              borderColor: theme.palette.primary.light,
+            },
+            // set hover khi click vao se chuyen mau nhe
+            "&:hover": {
+              ".MuiOutlinedInput-notchedOutline": {
+                borderColor: theme.palette.primary.main,
+              },
+              "& fieldset": {
+                borderWidth: "1px !important",
+              },
+            },
+          };
+        },
       },
     },
   },
