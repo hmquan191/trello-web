c9f5b9b4210fcfc387dc89879cdf47f256c11f1e
HMQ <hmquan191@gmail.com>
2025-03-10 20:00:06 +0700

    trello_board_content

--- src/pages/Boards/BoardContent/index.jsx
+++ src/pages/Boards/BoardContent/index.jsx
@@ -1,5 +1,35 @@
+import React from "react";
+
 import Box from "@mui/material/Box";
+
+import Tooltip from "@mui/material/Tooltip";
+
+import AddCardIcon from "@mui/icons-material/AddCard";
+import DragHandleIcon from "@mui/icons-material/DragHandle";
+import ExpandMoreIcon from "@mui/icons-material/ExpandMore";
+import Typography from "@mui/material/Typography";
+
+import Card from "@mui/material/Card";
+import CardActions from "@mui/material/CardActions";
+import CardContent from "@mui/material/CardContent";
+import CardMedia from "@mui/material/CardMedia";
+import GroupIcon from "@mui/icons-material/Group";
+import CommentIcon from "@mui/icons-material/Comment";
+import AttachmentIcon from "@mui/icons-material/Attachment";
+import { Button } from "@mui/material";
+const COLUMN_HEADER_HEIGHT = "50px";
+const COLUMN_FOOTER_HEIGHT = "56px";
+
 function BoardContent() {
+  const [anchorEl, setAnchorEl] = React.useState(null);
+  const open = Boolean(anchorEl);
+  const handleClick = (event) => {
+    setAnchorEl(event.currentTarget);
+  };
+  const handleClose = () => {
+    setAnchorEl(null);
+  };
+
   return (
     <Box
       sx={{
@@ -7,13 +37,412 @@ function BoardContent() {
           return theme.palette.mode === "dark" ? "#34495e" : "#1976d2";
         },
         width: "100%",
-        height: (theme) =>
-          `calc(100vh - ${theme.trello.appBarHeight} - ${theme.trello.boardBarHeight})`, // dung calc de tinh toan phan con lai cua Content
-        display: "flex",
-        alignItems: "center",
+        height: (theme) => theme.trello.boardContentHeight, // dung calc de tinh toan phan con lai cua Content
+        p: "10px 0",
       }}
     >
-      Board Content
+      <Box
+        sx={{
+          bgColor: "inherit", // kế thừa của thằng cha
+          width: "100%",
+          height: "100%",
+          display: "flex",
+          overflowX: "auto",
+          overflowY: "hidden",
+          "&::-webkit-scrollbar-track": { m: 2 },
+        }}
+      >
+        {/* Box Column 01*/}
+        <Box
+          sx={{
+            minWidth: "300px",
+            maxWidth: "300px",
+            backgroundColor: (theme) => {
+              return theme.palette.mode === "dark" ? "#333643" : "#ebecf0";
+            },
+            ml: 2, //margin left
+            borderRadius: "6px",
+            height: "fit-content", // fit theo content hien co
+            maxHeight: (theme) =>
+              `calc(${theme.trello.boardContentHeight} - ${theme.spacing(5)})`,
+          }}
+        >
+          {/* Box column header */}
+          <Box
+            sx={{
+              height: COLUMN_HEADER_HEIGHT,
+              p: 2,
+              display: "flex",
+              alignItems: "center",
+              justifyContent: "space-between",
+            }}
+          >
+            <Typography
+              variant="h6"
+              sx={{
+                fontSize: "1rem",
+                fontWeight: "bold",
+                cursor: "pointer",
+              }}
+            >
+              Column Title
+            </Typography>
+            <Box>
+              <Tooltip title="More options">
+                <ExpandMoreIcon
+                  sx={{ color: "text.primary", cusor: "pointer" }}
+                  id="basic-column-dropdown"
+                  aria-controls={
+                    open ? "basic-menu-column-dropdown" : undefined
+                  }
+                  aria-haspopup="true"
+                  aria-expanded={open ? "true" : undefined}
+                  onClick={handleClick}
+                />
+              </Tooltip>
+            </Box>
+          </Box>
+          {/* Box column 01 */}
+          <Box
+            sx={{
+              p: "0 5px", // padding
+              m: "0 5px", // margin
+              display: "flex",
+              flexDirection: "column",
+              gap: 1, // = 8px
+              overflowX: "hidden",
+              overflowY: "auto", // hiện thanh scroll
+              maxHeight: (theme) =>
+                `calc(
+                  ${theme.trello.boardContentHeight} - 
+                  ${theme.spacing(5)} - 
+                  ${COLUMN_HEADER_HEIGHT} -
+                  ${COLUMN_FOOTER_HEIGHT}
+                  )`,
+              "&::-webkit-scrollbar-thumb ": { backgroundColor: "ced0da" },
+              "&::-webkit-scrollbar-thumb:hover": {
+                backgroundColor: "#bfc2cf",
+              },
+            }}
+          >
+            <Card
+              sx={{
+                cursor: "pointer",
+                boxShadow: "0 1px 1px rgba(0,0,0,0.2)",
+                overflow: "unset",
+              }}
+            >
+              <CardMedia
+                sx={{ height: 140 }}
+                image="https://thanhnien.mediacdn.vn/Uploaded/trucdl/2022_03_23/albumredvelvetlapkyluc3-3573.jpg"
+                title="red velvet"
+              />
+              <CardContent sx={{ p: 1.5, "&:last-child": { p: 1.5 } }}>
+                <Typography>Red Velvet</Typography>
+              </CardContent>
+              {/* Theo chieu kim dong ho padding, top right bottom left */}
+              <CardActions sx={{ p: "0 4px 8px 4px" }}>
+                <Button startIcon={<GroupIcon />} size="small">
+                  10
+                </Button>
+                <Button startIcon={<CommentIcon />} size="small">
+                  15
+                </Button>
+                <Button startIcon={<AttachmentIcon />} size="small">
+                  10
+                </Button>
+              </CardActions>
+            </Card>
+            <Card
+              sx={{
+                boxShadow: "0 1px 1px rgba(0,0,0,0.2)",
+                overflow: "unset",
+              }}
+            >
+              <CardContent sx={{ p: 1.5, "&:last-child": { p: 1.5 } }}>
+                <Typography>Card 01</Typography>
+              </CardContent>
+            </Card>
+            <Card
+              sx={{
+                boxShadow: "0 1px 1px rgba(0,0,0,0.2)",
+                overflow: "unset",
+              }}
+            >
+              <CardContent sx={{ p: 1.5, "&:last-child": { p: 1.5 } }}>
+                <Typography>Card 01</Typography>
+              </CardContent>
+            </Card>
+            <Card
+              sx={{
+                boxShadow: "0 1px 1px rgba(0,0,0,0.2)",
+                overflow: "unset",
+              }}
+            >
+              <CardContent sx={{ p: 1.5, "&:last-child": { p: 1.5 } }}>
+                <Typography>Card 01</Typography>
+              </CardContent>
+            </Card>
+            <Card
+              sx={{
+                boxShadow: "0 1px 1px rgba(0,0,0,0.2)",
+                overflow: "unset",
+              }}
+            >
+              <CardContent sx={{ p: 1.5, "&:last-child": { p: 1.5 } }}>
+                <Typography>Card 01</Typography>
+              </CardContent>
+            </Card>{" "}
+            <Card
+              sx={{
+                boxShadow: "0 1px 1px rgba(0,0,0,0.2)",
+                overflow: "unset",
+              }}
+            >
+              <CardContent sx={{ p: 1.5, "&:last-child": { p: 1.5 } }}>
+                <Typography>Card 01</Typography>
+              </CardContent>
+            </Card>{" "}
+            <Card
+              sx={{
+                boxShadow: "0 1px 1px rgba(0,0,0,0.2)",
+                overflow: "unset",
+              }}
+            >
+              <CardContent sx={{ p: 1.5, "&:last-child": { p: 1.5 } }}>
+                <Typography>Card 01</Typography>
+              </CardContent>
+            </Card>{" "}
+            <Card
+              sx={{
+                boxShadow: "0 1px 1px rgba(0,0,0,0.2)",
+                overflow: "unset",
+              }}
+            >
+              <CardContent sx={{ p: 1.5, "&:last-child": { p: 1.5 } }}>
+                <Typography>Card 01</Typography>
+              </CardContent>
+            </Card>{" "}
+            <Card
+              sx={{
+                boxShadow: "0 1px 1px rgba(0,0,0,0.2)",
+                overflow: "unset",
+              }}
+            >
+              <CardContent sx={{ p: 1.5, "&:last-child": { p: 1.5 } }}>
+                <Typography>Card 01</Typography>
+              </CardContent>
+            </Card>{" "}
+            <Card
+              sx={{
+                boxShadow: "0 1px 1px rgba(0,0,0,0.2)",
+                overflow: "unset",
+              }}
+            >
+              <CardContent sx={{ p: 1.5, "&:last-child": { p: 1.5 } }}>
+                <Typography>Card 01</Typography>
+              </CardContent>
+            </Card>{" "}
+            <Card
+              sx={{
+                boxShadow: "0 1px 1px rgba(0,0,0,0.2)",
+                overflow: "unset",
+              }}
+            >
+              <CardContent sx={{ p: 1.5, "&:last-child": { p: 1.5 } }}>
+                <Typography>Card 01</Typography>
+              </CardContent>
+            </Card>{" "}
+            <Card
+              sx={{
+                boxShadow: "0 1px 1px rgba(0,0,0,0.2)",
+                overflow: "unset",
+              }}
+            >
+              <CardContent sx={{ p: 1.5, "&:last-child": { p: 1.5 } }}>
+                <Typography>Card 01</Typography>
+              </CardContent>
+            </Card>{" "}
+            <Card
+              sx={{
+                boxShadow: "0 1px 1px rgba(0,0,0,0.2)",
+                overflow: "unset",
+              }}
+            >
+              <CardContent sx={{ p: 1.5, "&:last-child": { p: 1.5 } }}>
+                <Typography>Card 01</Typography>
+              </CardContent>
+            </Card>{" "}
+            <Card
+              sx={{
+                boxShadow: "0 1px 1px rgba(0,0,0,0.2)",
+                overflow: "unset",
+              }}
+            >
+              <CardContent sx={{ p: 1.5, "&:last-child": { p: 1.5 } }}>
+                <Typography>Card 01</Typography>
+              </CardContent>
+            </Card>
+          </Box>
+
+          {/* Box footer */}
+          <Box
+            sx={{
+              height: COLUMN_FOOTER_HEIGHT,
+              p: 2,
+              display: "flex",
+              alignItems: "center",
+              justifyContent: "space-between",
+            }}
+          >
+            <Button startIcon={<AddCardIcon />}>Add new card</Button>
+            <Tooltip title="Drag to modify">
+              <DragHandleIcon sx={{ cursor: "pointer" }} />
+            </Tooltip>
+          </Box>
+        </Box>
+
+        {/* Box column 02 */}
+
+        <Box
+          sx={{
+            minWidth: "300px",
+            maxWidth: "300px",
+            backgroundColor: (theme) => {
+              return theme.palette.mode === "dark" ? "#333643" : "#ebecf0";
+            },
+            ml: 2, //margin left
+            borderRadius: "6px",
+            height: "fit-content", // fit theo content hien co
+            maxHeight: (theme) =>
+              `calc(${theme.trello.boardContentHeight} - ${theme.spacing(5)})`,
+          }}
+        >
+          {/* Box column header */}
+          <Box
+            sx={{
+              height: COLUMN_HEADER_HEIGHT,
+              p: 2,
+              display: "flex",
+              alignItems: "center",
+              justifyContent: "space-between",
+            }}
+          >
+            <Typography
+              variant="h6"
+              sx={{
+                fontSize: "1rem",
+                fontWeight: "bold",
+                cursor: "pointer",
+              }}
+            >
+              Column Title
+            </Typography>
+            <Box>
+              <Tooltip title="More options">
+                <ExpandMoreIcon
+                  sx={{ color: "text.primary", cusor: "pointer" }}
+                  id="basic-column-dropdown"
+                  aria-controls={
+                    open ? "basic-menu-column-dropdown" : undefined
+                  }
+                  aria-haspopup="true"
+                  aria-expanded={open ? "true" : undefined}
+                  onClick={handleClick}
+                />
+              </Tooltip>
+            </Box>
+          </Box>
+          {/* Box column 01 */}
+          <Box
+            sx={{
+              p: "0 5px", // padding
+              m: "0 5px", // margin
+              display: "flex",
+              flexDirection: "column",
+              gap: 1, // = 8px
+              overflowX: "hidden",
+              overflowY: "auto", // hiện thanh scroll
+              maxHeight: (theme) =>
+                `calc(
+                  ${theme.trello.boardContentHeight} - 
+                  ${theme.spacing(5)} - 
+                  ${COLUMN_HEADER_HEIGHT} -
+                  ${COLUMN_FOOTER_HEIGHT}
+                  )`,
+              "&::-webkit-scrollbar-thumb ": { backgroundColor: "ced0da" },
+              "&::-webkit-scrollbar-thumb:hover": {
+                backgroundColor: "#bfc2cf",
+              },
+            }}
+          >
+            <Card
+              sx={{
+                cursor: "pointer",
+                boxShadow: "0 1px 1px rgba(0,0,0,0.2)",
+                overflow: "unset",
+              }}
+            >
+              <CardMedia
+                sx={{ height: 140 }}
+                image="https://thanhnien.mediacdn.vn/Uploaded/trucdl/2022_03_23/albumredvelvetlapkyluc3-3573.jpg"
+                title="red velvet"
+              />
+              <CardContent sx={{ p: 1.5, "&:last-child": { p: 1.5 } }}>
+                <Typography>Red Velvet</Typography>
+              </CardContent>
+              {/* Theo chieu kim dong ho padding, top right bottom left */}
+              <CardActions sx={{ p: "0 4px 8px 4px" }}>
+                <Button startIcon={<GroupIcon />} size="small">
+                  10
+                </Button>
+                <Button startIcon={<CommentIcon />} size="small">
+                  15
+                </Button>
+                <Button startIcon={<AttachmentIcon />} size="small">
+                  10
+                </Button>
+              </CardActions>
+            </Card>
+            <Card
+              sx={{
+                boxShadow: "0 1px 1px rgba(0,0,0,0.2)",
+                overflow: "unset",
+              }}
+            >
+              <CardContent sx={{ p: 1.5, "&:last-child": { p: 1.5 } }}>
+                <Typography>Card 01</Typography>
+              </CardContent>
+            </Card>
+            <Card
+              sx={{
+                boxShadow: "0 1px 1px rgba(0,0,0,0.2)",
+                overflow: "unset",
+              }}
+            >
+              <CardContent sx={{ p: 1.5, "&:last-child": { p: 1.5 } }}>
+                <Typography>Card 01</Typography>
+              </CardContent>
+            </Card>
+          </Box>
+
+          {/* Box footer */}
+          <Box
+            sx={{
+              height: COLUMN_FOOTER_HEIGHT,
+              p: 2,
+              display: "flex",
+              alignItems: "center",
+              justifyContent: "space-between",
+            }}
+          >
+            <Button startIcon={<AddCardIcon />}>Add new card</Button>
+            <Tooltip title="Drag to modify">
+              <DragHandleIcon sx={{ cursor: "pointer" }} />
+            </Tooltip>
+          </Box>
+        </Box>
+      </Box>
     </Box>
   );
 }
--- src/theme.js
+++ src/theme.js
@@ -3,10 +3,15 @@ import { experimental_extendTheme as extendTheme } from "@mui/material/styles";
 import { deepOrange, orange, teal, cyan, red } from "@mui/material/colors";
 import { BorderColor } from "@mui/icons-material";
 
+const APP_BAR_HEIGHT = "58px";
+const BOARD_BAR_HEIGHT = "60px";
+const BOARD_CONTENT_HEIGHT = `calc(100vh - ${APP_BAR_HEIGHT} - ${BOARD_BAR_HEIGHT})`; // dung calc de tinh toan phan con lai cua Content
+
 const theme = extendTheme({
   trello: {
-    appBarHeight: "58px",
-    boardBarHeight: "60px",
+    appBarHeight: APP_BAR_HEIGHT,
+    boardBarHeight: BOARD_BAR_HEIGHT,
+    boardContentHeight: BOARD_CONTENT_HEIGHT,
   },
 
   colorSchemes: {
@@ -39,6 +44,7 @@ const theme = extendTheme({
           "*::-webkit-scrollbar-thumb:hover": {
             backgroundColor: "#white",
           },
+          "*::-webkit-scrollbar-track": { m: 2 },
         },
       },
     },
@@ -56,6 +62,13 @@ const theme = extendTheme({
       styleOverrides: {
         root: {
           fontSize: "0.875rem",
+        },
+      },
+    },
+    MuiTypography: {
+      styleOverrides: {
+        root: {
+          "&.MuiTypography-body1": { fontSize: "0.875rem" },
         },
       },
     },
